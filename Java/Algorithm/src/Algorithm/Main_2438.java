8e1e4174ab8e329dc2d44893e1559483528da7b3
이소영 <th42500@naver.com>
2021-09-04 03:02:31 +0900

    2021.09.23 study

---
+++ Java/Algorithm/src/Algorithm/Main_2438.java
@@ -0,0 +1,20 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2438 {  // 별 찍기 - 1
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+	}
+
+}
---
+++ Java/Algorithm/src/Algorithm/Main_2742.java
@@ -0,0 +1,16 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Main_2742 {  // 기찍 N
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		for (int i = N; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+
+}

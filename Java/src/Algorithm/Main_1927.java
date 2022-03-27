7a1b5e3e8eed9c09ba35e4cb99183c61b34a4f4e
이소영 <th42500@naver.com>
2022-03-27 23:13:43 +0900

    2022.03.27 study

---
+++ Java/src/Algorithm/Main_1927.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main_1927 {  // 최소 힙
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			int number = Integer.parseInt(br.readLine());
+			if(number == 0) {
+				// System.out.println("현재 수는 " + i);
+				if(!q.isEmpty()) {  // q가 비어있지 않다면
+					System.out.println(q.poll());  // 가장 작은 수 poll() & 출력
+				}else {
+					System.out.println(0);
+				}	
+			} else {
+				q.add(number);
+			}
+		}
+	}
+}

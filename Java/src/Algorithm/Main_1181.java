f067654e934df4cf764c5651f817cfa50168efb8
이소영 <th42500@naver.com>
2022-06-20 23:58:28 +0900

    2022.06.20 study

---
+++ Java/src/Algorithm/Main_1181.java
@@ -0,0 +1,43 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main_1181 {  // 단어 정렬
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] dict = new String[N];
+		
+		for (int i = 0; i < N; i++) {
+			dict[i] = br.readLine();
+		}
+		
+		Arrays.sort(dict, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length() == o2.length()) {
+					for (int i = 0; i < o1.length(); i++) {
+						if(o1.charAt(i) > o2.charAt(i)) {
+							return 1;
+						}else if(o1.charAt(i) < o2.charAt(i)) {
+							return -1;
+						}
+					}
+				}
+				return o1.length() - o2.length();  // 길이가 짧은 것부터
+			}
+			
+		});
+		
+		System.out.println(Arrays.toString(dict));
+	}
+
+}

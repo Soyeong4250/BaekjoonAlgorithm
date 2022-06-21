aa56dc9d8676335bc21e197f2e390fd38b400883
이소영 <th42500@naver.com>
2022-06-22 00:04:21 +0900

    2022.06.22 study

--- Java/src/Algorithm/Main_11650.java
+++ Java/src/Algorithm/Main_11650.java
@@ -1,10 +1,37 @@
 package Algorithm;
 
-public class Main_11650 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main_11650 {  // 좌표 정렬하기
 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] spot = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				spot[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Arrays.sort(spot, (o1, o2) -> {
+			if(o1[0] == o2[0]) {
+				return Integer.compare(o1[1], o2[1]);
+			} else {
+				return Integer.compare(o1[0], o2[0]);
+			}
+		});
+		
+		for (int i = 0; i < spot.length; i++) {
+			System.out.println(spot[i][0] + " " + spot[i][1]);
+		}
 	}
 
 }

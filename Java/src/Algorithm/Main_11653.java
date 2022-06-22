475d52b558adf3ff68e9232154fd74e376daa5a6
이소영 <th42500@naver.com>
2022-06-22 23:55:19 +0900

    2022.06.22 study

---
+++ Java/src/Algorithm/Main_11653.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_11653 {  // 소인수분해
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int num = 2;
+		while(N != 1) {
+			if(N%num == 0) {
+				System.out.println(num);
+				N /= num;
+			} else {
+				num++;
+			}
+		}
+	}
+
+}

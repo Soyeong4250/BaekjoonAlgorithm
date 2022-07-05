b20b19253dee1ae69ebf15606ca7e24eb31ddbc7
이소영 <th42500@naver.com>
2022-07-05 23:49:28 +0900

    2022.07.05 study

---
+++ Java/src/Algorithm/Main_2750.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2750 {  //  수 정렬하기
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] num = new int[N];
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(num);
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
+		}
+	}
+}

ac0f51b4db875d9b9625d35817d7c4783e478975
이소영 <th42500@naver.com>
2022-07-12 23:58:26 +0900

    2022.07.12 study

---
+++ Java/src/Algorithm/Main_1193.java
@@ -0,0 +1,43 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1193 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		int numerator = 1;
+		int denominator = 1;
+		
+		int[] ndir = {1, -1};
+		int[] ddir = {-1, 1};
+		
+		if(X == 1) {
+			System.out.println("1/1");
+			return;
+		}
+		
+		int d = 0;
+		for (int i = X-2; i >= 0; i--) {
+			if(numerator == 1) {
+				denominator += 1;
+				d = 0;
+				continue;
+			}
+			
+			if(denominator == 1) {
+				numerator += 1;
+				d = 1;
+				continue;
+			}
+			
+			denominator += ndir[d];
+			numerator += ddir[d];
+		}
+	}
+
+}

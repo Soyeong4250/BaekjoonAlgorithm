05da6da6cb238a8cbe11b1b40db6f7c31e1de503
이소영 <th42500@naver.com>
2022-09-07 16:44:37 +0900

    2022.09.07 study

--- Java/src/Algorithm/Main_17298.java
+++ Java/src/Algorithm/Main_17298.java
@@ -3,49 +3,49 @@ package Algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main_17298 {  // 오큰수
+public class Main_17298 { // 오큰수
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
 		
-		Stack<Integer>stack = new Stack<>();
+		Stack<Integer> stack = new Stack<>();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		StringBuilder sb  = new StringBuilder();
-		int num = 0;
-		for (int i = 0; i < N; i++) {
-			num = Integer.parseInt(st.nextToken());
-			if(!stack.isEmpty() && stack.peek() < num && i != N-1) {
-				sb.append(num + " ");
-				stack.pop();
-				stack.push(num);
-			} else {
-				stack.push(num);
-			}
-			System.out.println("num은 " + num);
-			System.out.println(stack);
-			System.out.println(sb.toString());
-		}
-		
-		while(!stack.isEmpty()) {
-			if(stack.peek() < num) {
-				
-			} else {
-				
-			}
-		}
-		
 		int[] result = new int[N];
+		Arrays.fill(result, -1);
+		
 		for (int i = 0; i < N; i++) {
-			
+			int num = Integer.parseInt(st.nextToken());
+			System.out.println(num);
+			if(!stack.isEmpty()&&stack.peek()<nums) {
+				System.out.println("들어왔습니다.");
+				result[i-1] = num;
+			} else if(!stack.isEmpty()){
+				
+			}
+			stack.add(num);
+			System.out.println("stack의 마지막은 " + stack.peek());
+			System.out.println("stack에 " +num + " 추가");
 		}
+		System.out.println(Arrays.toString(result));
+		
+		sb = new StringBuilder();
+		for(int i=0; i<N; i++) {
+			if(i < N-1 && result[i] == -1) {
+				result[i] = result[i+1];
+			} 
+			sb.append(result[i] + " ");
+		}
+		
+		System.out.println(sb.toString().trim());
 	}
 
 }
--- Java/src/Algorithm/Main_18352.java
+++ Java/src/Algorithm/Main_18352.java
@@ -4,6 +4,7 @@ public class Main_18352 {  // 특정 거리의 도시 찾기
 
 	public static void main(String[] args) {
 		
+		
 	}
 
 }

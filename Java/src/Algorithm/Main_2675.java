fe3d141b63cda0e84c32f5cbb6035f756d71f253
이소영 <th42500@naver.com>
2022-09-11 23:58:51 +0900

    2022.09.11 study

---
+++ Java/src/Algorithm/Main_2675.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2675 {  // 문자열 반복
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int cnt = Integer.parseInt(st.nextToken());
+			String S = st.nextToken();
+			
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < S.length(); i++) {
+				
+			}
+			
+		}
+		
+		
+		
+	}
+
+}

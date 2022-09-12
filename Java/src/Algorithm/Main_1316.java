dddb2d15ef6b9f22bd68c227b861e7e5f3cbc2d0
이소영 <th42500@naver.com>
2022-09-12 23:51:17 +0900

    2022.09.12 study

--- Java/src/Algorithm/Main_1316.java
+++ Java/src/Algorithm/Main_1316.java
@@ -3,7 +3,7 @@ package Algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main_1316 {  // 그룹 단어 체커
 
@@ -11,25 +11,29 @@ public class Main_1316 {  // 그룹 단어 체커
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int cnt = N;
+		int result = 0;
 		for (int i = 0; i < N; i++) {
-			cnt += check(br.readLine());
-		}
-		
-	}
-
-	private static int check(String s) {
-		HashMap<String, Integer> hash = new HashMap<>();
-		for (int i = 0; i < s.length()-1; i++) {
-			String key = s.substring(i, i+1);
-			if(i > 0 && !s.substring(i-1, i).equals(key) && hash.get(key) != 0) {
-				return -1;
+			boolean[] alphabet = new boolean[26];
+			String word = br.readLine();
+			boolean check = true;
+			for (int j = 0; j < word.length(); j++) {
+				char ch = word.charAt(j);
+				if(j > 0 && ch!= word.charAt(j-1) && alphabet[(int)ch - 97]) {
+					check = false;
+					break;
+				}
+				alphabet[(int)ch - 97] = true;
 			}
-			hash.put(key, hash.getOrDefault(key, 0) + 1);
-			System.out.println(hash);
+			
+			if(check) {
+//				System.out.println(word);
+				result++;
+			}
+			
 		}
 		
-		return 1;
+		System.out.println(result);
+		
 	}
 
 }

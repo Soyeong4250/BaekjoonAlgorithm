6dece46ec9ce84a51a714b18fa0532146729c612
이소영 <th42500@naver.com>
2022-09-13 23:51:26 +0900

    2022.09.13 study

--- Java/src/Algorithm/Main_10757.java
+++ Java/src/Algorithm/Main_10757.java
@@ -3,7 +3,6 @@ package Algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main_10757 {  // 큰 수 A + B
@@ -12,7 +11,23 @@ public class Main_10757 {  // 큰 수 A + B
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		String A = st.nextToken();
+		String B = st.nextToken();
 		
+		int aIdx = A.length()-1;
+		int bIdx = B.length()-1;
+		StringBuilder sb = new StringBuilder();
+		int num = 0;
+		while(aIdx >= 0 && bIdx >= 0) {
+			int sum = (A.charAt(aIdx) - '0') + (B.charAt(bIdx) - '0');
+			sb.append(sum + num);
+			if(sum >= 10) {
+				num = sum/10;
+			}
+			aIdx--;
+			bIdx--;
+			System.out.println(sb.toString());
+		}
 	}
 
 }

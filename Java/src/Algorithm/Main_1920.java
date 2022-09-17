6ee7246e4889317147e78df3af4f20cee40413e4
이소영 <th42500@naver.com>
2022-09-18 04:16:52 +0900

    2022.09.17 study

--- Java/src/Algorithm/Main_1920.java
+++ Java/src/Algorithm/Main_1920.java
@@ -17,29 +17,38 @@ public class Main_1920 {  // 수 찾기
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
+		Arrays.sort(arr);  // 1. N개의 수 오름차순 정렬
 		
 		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		int[] arr2 = new int[M];
-		for (int i = 0; i < arr.length; i++) {
-			arr2[i] = Integer.parseInt(st.nextToken());
-		}
 		
+		// 2. 이진탐색
 		for (int i = 0; i < M; i++) {
-			int result = 0;
-			int idx = N/2;
-			while(0 <= idx && idx < N) {
-				if(arr2[i] == arr[idx]) {
-					result = 1;
+			int target = Integer.parseInt(st.nextToken());
+			// 2-1. 중앙값(인덱스) 정하기
+			int start = 0;
+			int mid = 0;
+			int end = N-1;
+			
+			boolean flag = false;
+			while(start <= end) {
+				mid = (start+end)/2;
+				if(arr[mid] == target) {
+					flag = true;
 					break;
-				}else if(arr2[i] < arr[idx]) {
-					idx -= idx/2;
-				}else {
-					idx += idx/2;
+				}else if(arr[mid] < target) {  // 정렬된 배열 mid 인덱스의 값보다 target이 크다면
+					start = mid+1;
+				}else if(arr[mid] > target) {  // 정렬된 배열 mid 인덱스의 값이 target보다 크다면
+					end = mid-1;
 				}
 			}
-			System.out.println(result);
+			
+			if(flag) {
+				System.out.println(1);
+			}else {
+				System.out.println(0);
+			}
+			
 		}
 	}
 

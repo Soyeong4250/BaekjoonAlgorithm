1414b878274e1f30e5869f00eaff252fc7bbd769
이소영 <th42500@naver.com>
2022-09-26 23:56:27 +0900

    2022.09.26 study

---
+++ Java/src/Algorithm/Main_1377.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class Main_1377 {  // 버블 소트
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		int[] arr2 = new int[N];
+		
+		for (int i = 0; i < arr.length; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[i] = num;
+			arr2[i] = num;
+		}
+		
+		Arrays.sort(arr2);  // 정렬
+		
+		
+	}
+
+}

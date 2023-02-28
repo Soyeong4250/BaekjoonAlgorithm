8faf23ca55dd2f5a703f8d367e6720ca8055b11d
이소영 <th42500@naver.com>
2023-02-28 17:03:28 +0900

    [Feat] 2023.02.28 - RGB 거리 풀이 완료

--- Java/src/Algorithm/Main1149.java
+++ Java/src/Algorithm/Main1149.java
@@ -9,43 +9,33 @@ import java.util.StringTokenizer;
 public class Main1149 { // RGB 거리
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		Main1149 main = new Main1149();
-
-		System.out.println(main.solution());
-	}
-
-	private int solution() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int answer = -1;
-		
-		char[] color = new char[n];
-		int[] sum = new int[n];
-		
+
+		int[][] home = new int[n][3];
+
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
+
 			int r = Integer.parseInt(st.nextToken());
 			int g = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-			if(r <= g && r <= b) {
-				color[i] = 'r';
-				sum[i] = r;
-			}else if(g <= r && g <= b) {
-				color[i] = 'g';
-				sum[i] = g;
-			}else if(b <= r && b <= g) {
-				color[i] = 'b';
-				sum[i] = b;
+
+			if (i == 0) {
+				home[i][0] = r;
+				home[i][1] = g;
+				home[i][2] = b;
+			} else {
+				home[i][0] = Math.min(home[i - 1][1], home[i - 1][2]) + r;
+				home[i][1] = Math.min(home[i - 1][0], home[i - 1][2]) + g;
+				home[i][2] = Math.min(home[i - 1][0], home[i - 1][1]) + b;
 			}
-			
 		}
 		
-		System.out.println(Arrays.toString(color));
-		System.out.println(Arrays.toString(sum));
+		for (int i = 0; i < home.length; i++) {
+			System.out.println(Arrays.toString(home[i]));
+		}
 		
-		return sum[n-1];
+		System.out.println(Math.min(home[n-1][0], Math.min(home[n-1][1], home[n-1][2])));
 	}
-
 }

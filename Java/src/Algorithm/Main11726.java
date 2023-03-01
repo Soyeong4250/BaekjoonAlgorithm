5bd11f5902687012d384e7efb7acc8d4eceb4977
이소영 <th42500@naver.com>
2023-03-01 23:17:04 +0900

    [Feat] 2023.03.01 - 2xn 타일링 풀이 완료

---
+++ Java/src/Algorithm/Main11726.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main11726 {  // 2 X n 타일링
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Main11726 main = new Main11726();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(main.solution(n));
+	}
+
+	private int solution(int n) {
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+		}
+		
+		return dp[n];
+	}
+
+}

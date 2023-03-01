ae507af24eca7786147ac6461962ec10cb017fd0
이소영 <th42500@naver.com>
2023-03-01 23:39:53 +0900

    [Refactor] 2023.03.01 - 2xn 타일링 코드 수정

--- Java/src/Algorithm/Main11726.java
+++ Java/src/Algorithm/Main11726.java
@@ -16,11 +16,11 @@ public class Main11726 {  // 2 X n 타일링
 	}
 
 	private int solution(int n) {
-		int[] dp = new int[1001];
+		int[] dp = new int[n+2];
 		dp[1] = 1;
 		dp[2] = 2;
 		
-		for (int i = 3; i < dp.length; i++) {
+		for (int i = 3; i <= n; i++) {
 			dp[i] = (dp[i-1] + dp[i-2]) % 10007;
 		}
 		
